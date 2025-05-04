a92e6b89b886ae61ceffed2b362bc3155ed4ff2f
RakelyDias <161658315+RakelyDias@users.noreply.github.com>
2025-05-04 15:23:08 -0300

    Create exemplo4.js

---
+++ exemplo4.js
@@ -0,0 +1,11 @@
+function multiplicacao(numero1, numero2) {
+  return numero1 * numero2;
+}
+
+let multiplicacao1 = multiplicacao(6, 7);
+let multiplicacao2 = multiplicacao(9, 10);
+let multiplicacao3 = multiplicacao(12, 25);
+
+console.log(multiplicacao1);
+console.log(multiplicacao2);
+console.log(multiplicacao3);
